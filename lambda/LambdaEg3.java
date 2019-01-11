951005c08b45619bf6ed18930ab5f5ee5e797213
Jagriti <jagritisrvstv@gmail.com>
2019-01-11 12:03:02 +0545

    Default Changelist

--- lambda/LambdaEg3.java
+++ lambda/LambdaEg3.java
@@ -1,24 +1,52 @@
 package lambda;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
  * Created by Jagriti on 1/8/2019.
  */
 public class LambdaEg3 {
+    interface Test{
+        public int thisIstest(int a);
+    }
     public static void main(String[] args) {
-        long result = 0;
-        long modulo = (long) 10000000000.0;
-
-        for (int i = 1; i <= 1000; i++) {
-            long temp = i;
-            for (int j = 1; j < i; j++) {
-                temp *= i;
-                System.out.println("chec1 "+temp);
-                temp %= modulo;
-                System.out.println("check 2"+temp);
+        Test t  = (a)->{
+            if(a>0){
+                return a;
+            }else{
+                return 0;
             }
-            result += temp;
-            result %= modulo;
-        }
-        System.out.println(result);
+        };
+        int c = t.thisIstest(5);
+        System.out.println(c);
+
+        /*LAMBDA FOR LOOPING*/
+        List<String> list=new ArrayList<String>();
+        list.add("Rick");
+        list.add("Negan");
+        list.add("Daryl");
+        list.add("Glenn");
+        list.add("Carl");
+        list.forEach(
+                // lambda expression
+                (names) -> System.out.println(names)
+        );
+        /*LAMBDA FOR HASHMAPS*/
+        Map<String, Integer> prices = new HashMap<>();
+        prices.put("Apple", 50);
+        prices.put("Orange", 20);
+        prices.put("Banana", 10);
+        prices.put("Grapes", 40);
+        prices.put("Papaya", 50);
+		/* Iterate without using Lambda
+		   for (Map.Entry<String, Integer> entry : prices.entrySet()) {
+		   System.out.println("Fruit: " + entry.getKey() + ", Price: " + entry.getValue());
+		   }
+		*/
+        prices.forEach((k,v)->System.out.println("Fruit: " + k + ", Price: " + v));
+
     }
 }
