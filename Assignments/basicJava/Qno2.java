b90d253b30132d7e7fe5d2dab95f4373a7d71e39
Jagriti <jagritisrvstv@gmail.com>
2019-01-22 16:42:28 +0545

    Default Changelist

---
+++ Assignments/basicJava/Qno2.java
@@ -0,0 +1,28 @@
+package Assignments.basicJava;
+
+import java.util.Scanner;
+
+/**
+ * Created by Jagriti on 1/21/2019.
+ */
+
+/**
+show the following output from the following input
+        input
+        java 100
+        cpp 65
+        python 50
+        output
+        ================================
+        java           100
+        cpp            065
+        python         050
+        ================================
+ */
+public class Qno2 {
+    public static void main(String[] args) {
+
+    }
+}
+
+
