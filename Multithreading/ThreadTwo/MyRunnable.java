02494212264feeb92c5265ed8027bab7afbd2462
Jagriti <jagritisrvstv@gmail.com>
2019-03-29 11:31:06 +0545

    Default Changelist

---
+++ Multithreading/ThreadTwo/MyRunnable.java
@@ -0,0 +1,12 @@
+package Multithreading.ThreadTwo;
+
+import static Multithreading.ThreadTwo.ThreadColor.ANSI_RED;
+
+
+public class MyRunnable implements Runnable {
+
+    @Override
+    public void run() {
+        System.out.println(ANSI_RED + "Hello from MyRunnable's implementation of run()");
+    }
+}
